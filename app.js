073d30296940bd1c607a27c2ec40a7f1a5a4c410
Jos <josue9499@gmail.com>
2021-05-22 20:23:31 -0500

    changed function to es6 syntax

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let contador=0
 let message= "Ya no le piques, le duele :c"
 
-function countingClicks(){
+const countingClicks = () => {
     document.getElementById("counting").innerHTML = ++contador
     if(contador > 10){
         document.getElementById("warning").innerHTML = message
