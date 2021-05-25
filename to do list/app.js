5f47f133b352b308c68d01a7620e342456f839bf
Jorge Sanchez <jrgels99@gmail.com>
2021-05-25 03:40:12 -0500

    Ya se puede ver el la interfaz grafica del proyecto todolist, y guarda las treas.

--- to do list/app.js
+++ to do list/app.js
@@ -4,9 +4,17 @@ const listaTarea = document.getElementById("lista-tareas")
 const template = document.getElementById("template").content
 const fragment = document.createDocumentFragment()
 
+//Array con las tareas
 let tareas = {}
 
-console.log(Date.now())
+//Para que al refrescar la pag no se pierdan las tareas ya agendadas
+document.addEventListener('DOMContentLoaded', ()=>{
+    pintarTareas()
+})
+
+listaTarea.addEventListener('click', e => {
+    btnAccion(e)
+})
 
 formulario.addEventListener("submit", e => {
     e.preventDefault()
@@ -15,6 +23,55 @@ formulario.addEventListener("submit", e => {
     setTarea(e)
 })
 
+//Funcion para guardar la tarea
 const setTarea = e => {
-    
+    if(input.value.trim()===''){
+        console.log('esta vacio')
+        alert("🖕🏻 Intriduce un valor! 🖕🏻")
+        return
+    }
+    console.log('diste click')
+
+    const tarea = {
+        id: Date.now(),
+        texto: input.value,
+        estado: false,
+
+    }
+
+    tareas[tarea.id] = tarea
+
+    //console.log(tareas);
+
+    formulario.reset()
+    input.focus()
+
+    pintarTareas()
+}
+
+const pintarTareas = () => {
+    //Para que limpie lo que estaba antes (parte desde 0)
+    listaTarea.innerHTML = ''
+    //Accedemos a los onjetos
+    Object.values(tareas).forEach(tarea => {
+        //Clonamos el template
+        const clone = template.cloneNode(true)
+        //Pintamos dentro del parrafo la tarea
+        clone.querySelector('p').textContent = tarea.texto
+        clone.querySelector('.fas').dataset.id = tarea.id
+        //Para pintarlo (INVESTIGAR MAS A DETALLE)
+        fragment.appendChild(clone)
+    })
+
+    listaTarea.appendChild(fragment)
+}
+
+const btnAccion = e => {
+    //para detectar a que boton le picas e.target.classList.contains('fa-check-circle'
+    if(e.target.classList.contains('fa-check-circle')){
+        tareas[e.target.dataset.id].estado = true
+        pintarTareas()
+        console.log(tareas)
+    }
+    e.stopPropagation()
 }
